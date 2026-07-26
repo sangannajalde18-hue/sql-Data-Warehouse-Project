0f16403e5e95ae7eb28c98f7c16ef4beb6e3b3bf
Sanganna Jalade <sangannajalde18@gmail.com>
2026-07-26 13:16:41 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,22 @@
+
+-- Create database 'DataWarehouse'
+
+USE master;
+
+
+CREATE DATABASE DataWarehouse;
+
+-- Use database
+USE DataWarehouse;
+
+
+
+-- Create Schemas
+
+GO
+CREATE  SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
